6798d9efd42c73c131a382129395787fcf4c4666
TheMGLegends <matejovo34@gmail.com>
2023-10-25 10:28:57 +0100

    Tutorial 5 Completed

--- CGP_Week5_Exercises/main.cpp
+++ CGP_Week5_Exercises/main.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 #include <stdlib.h>
 #include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_mixer.h"
+#include "SDL_ttf.h"
 
 SDL_Window* g_sdlWindow;
 SDL_Renderer* g_sdlRenderer;
+TTF_Font* g_font;
 
-SDL_Texture* LoadTexture(const char* filename) 
+const int SCREEN_W = 1000;
+const int SCREEN_H = 1000;
+
+SDL_Texture* LoadTexture(const char* filename)
 {
 	// TODO: Load door.bmp as a surface
-	SDL_Surface* image = SDL_LoadBMP(filename);
+	//SDL_Surface* image = SDL_LoadBMP(filename);
+	SDL_Surface* image = IMG_Load(filename);
 	// TODO: Error checking
 	if (image == nullptr)
 	{
-		std::cout << "Failed to load " << filename << " .SDL error : " << SDL_GetError() << std::endl;
+		std::cout << "Failed to load " << filename << " .SDL error : " << IMG_GetError() << std::endl;
 	}
 
 	// TODO: Convert surface of the loaded bmp into a texture
@@ -29,19 +37,7 @@ SDL_Texture* LoadTexture(const char* filename)
 	return texture;
 }
 
-const int SCREEN_W = 1000;
-const int SCREEN_H = 1000;
-
-int main(int argc, char* argv[])
-{
-	srand(unsigned int(time(NULL)));
-
-	// TODO: Initialise all SDL subsystems
-	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
-	{
-		std::cout << "Failed to initialise SDL. SDL error: " << SDL_GetError() << std::endl;
-	}
-
+bool Initialise() {
 	// TODO: Create a window with the specified name, anywhere on the screen, 800x600 pixel size and no SDL_WindowFlags flags.
 	g_sdlWindow = SDL_CreateWindow("Week 1 - Intro and Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_W, SCREEN_H, NULL);
 
@@ -49,6 +45,7 @@ int main(int argc, char* argv[])
 	if (g_sdlWindow == nullptr)
 	{
 		std::cout << "Failed to create window. SDL error: " << SDL_GetError() << std::endl;
+		return true;
 	}
 
 	// TODO: Create a rendering context for our window. Enable hardware acceleration with the SDL_RENDERER_ACCELERATED flag
@@ -57,10 +54,101 @@ int main(int argc, char* argv[])
 	if (g_sdlRenderer == nullptr)
 	{
 		std::cout << "Failed to create renderer. SDL error: " << SDL_GetError() << std::endl;
+		return true;
+	}
+	
+	// TODO: Initialise all SDL subsystems
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		std::cout << "Failed to initialise SDL. SDL error: " << SDL_GetError() << std::endl;
+		return true;
 	}
 
-	SDL_Texture* doorTexture = LoadTexture("Assets/door.bmp");
-	SDL_Texture* crusaderTexture = LoadTexture("Assets/crusader.bmp");
+	// TODO: Initialise SDL Image
+	int flags = IMG_INIT_PNG | IMG_INIT_JPG;
+	int initted = IMG_Init(flags);
+
+	if (initted != flags) 
+	{
+		std::cout << "Failed to initialise SDL_Image error: " << IMG_GetError() << std::endl;
+		return true;
+	}
+
+	// TODO: Initialise SDL Mixer
+	flags = MIX_INIT_MP3;
+	initted = Mix_Init(flags);
+
+	if (initted != flags)
+	{
+		std::cout << "Failed to initialise SDL_Mixer error: " << Mix_GetError() << std::endl;
+		return true;
+	}
+
+	// TODO: Open Audio Device
+	if (Mix_OpenAudio(44100, AUDIO_S16SYS, 2, 2048) < 0) 
+	{
+		std::cout << "Failed to open audio device. SDL_Mixer error: " << Mix_GetError() << std::endl;
+		return true;
+	}
+
+	// TODO: Initialise SDL TTF
+	if (TTF_Init() < 0) {
+		std::cout << "Failed to initialise TTF. SDL_TTF error: " << TTF_GetError() << std::endl;
+		return true;
+	}
+
+	// TODO: Load Font
+	g_font = TTF_OpenFont("Assets/8bitOperatorPlus8-Regular.ttf", 32);
+	if (g_font == nullptr) {
+		std::cout << "TTF failed to load font. SDL_TTF error: " << TTF_GetError() << std::endl;
+		return true;
+	}
+
+	return false;
+}
+
+void CleanUp() {
+	// TODO: Clean up
+	//SDL_DestroyTexture(doorTexture);
+	//SDL_DestroyTexture(crusaderTexture);
+	SDL_DestroyRenderer(g_sdlRenderer); // Opposite of SDL_CreateRenderer(...)
+	SDL_DestroyWindow(g_sdlWindow); // Opposite of SDL_CreateWindow(...)
+	IMG_Quit();
+	Mix_CloseAudio();
+	Mix_Quit();
+	TTF_CloseFont(g_font);
+	TTF_Quit();
+	SDL_Quit(); // Opposite of SDL_Init(...)
+
+}
+
+
+int main(int argc, char* argv[])
+{
+	srand(unsigned int(time(NULL)));
+
+	if (Initialise()) {
+		std::cout << "Application failed to initialise. Quitting..." << std::endl;
+		return -1;
+	}
+
+	// TODO: Load SFX
+	Mix_Chunk* coinSFX = Mix_LoadWAV("Assets/Coin01.wav");
+
+	// TODO: Load Music
+	Mix_Music* music = Mix_LoadMUS("Assets/rng_lo-fi_loop.mp3");
+	// TODO: Music playing infinitely
+	Mix_PlayMusic(music, -1);
+
+
+	// TODO: Create texture:
+	SDL_Surface* textSurface = TTF_RenderText_Blended(g_font, "Hello World", { 255, 255, 255, 255 });
+	SDL_Texture* textTexture = SDL_CreateTextureFromSurface(g_sdlRenderer, textSurface);
+	SDL_FreeSurface(textSurface);
+
+	//SDL_Texture* doorTexture = LoadTexture("Assets/door.bmp");
+	//SDL_Texture* crusaderTexture = LoadTexture("Assets/crusader.bmp");
+	SDL_Texture* pngTexture = LoadTexture("Assets/sword.png");
 	//SDL_Texture* fishTexture = LoadTexture("Assets/fish_blue1.bmp");
 
 	// TODO: Creating texture without a surface
@@ -109,7 +197,7 @@ int main(int argc, char* argv[])
 			if (keyD) keyboardX += movementSpeed;
 
 			if (sdlEvent.type == SDL_QUIT) keepRunning = false;
-			if (sdlEvent.type == SDL_KEYDOWN) 
+			if (sdlEvent.type == SDL_KEYDOWN)
 			{
 				switch (sdlEvent.key.keysym.sym)
 				{
@@ -121,7 +209,7 @@ int main(int argc, char* argv[])
 				default: break;
 				}
 			}
-			if (sdlEvent.type == SDL_KEYUP) 
+			if (sdlEvent.type == SDL_KEYUP)
 			{
 				switch (sdlEvent.key.keysym.sym)
 				{
@@ -151,62 +239,65 @@ int main(int argc, char* argv[])
 			if (state[SDL_SCANCODE_D]) {
 				keyboardX += movementSpeed;
 			}
+			if (state[SDL_SCANCODE_SPACE]) {
+				Mix_PlayChannel(-1, coinSFX, 0);
+			}
 
 			switch (sdlEvent.type)
 			{
 			case SDL_QUIT:
 				keepRunning = false;
 				break;
-			/*
-			case SDL_KEYDOWN:
-				if (sdlEvent.key.keysym.sym == SDLK_ESCAPE)
-				{
-					keepRunning = false;
-				}
-				// ONLY UP
-				else if (sdlEvent.key.keysym.sym == SDLK_w)
-				{
-					keyW = 1;
-				}
-				// ONLY DOWN
-				else if (sdlEvent.key.keysym.sym == SDLK_s)
-				{
-					keyS = 1;
-				}
-				// ONLY LEFT
-				else if (sdlEvent.key.keysym.sym == SDLK_a)
-				{
-					keyA = 1;
-				}
-				// ONLY RIGHT
-				else if (sdlEvent.key.keysym.sym == SDLK_d)
-				{
-					keyD = 1;
-				}
-				break;
-			case SDL_KEYUP:
-				// ONLY UP
-				if (sdlEvent.key.keysym.sym == SDLK_w)
-				{
-					keyW = 0;
-				}
-				// ONLY DOWN
-				else if (sdlEvent.key.keysym.sym == SDLK_s)
-				{
-					keyS = 0;
-				}
-				// ONLY LEFT
-				else if (sdlEvent.key.keysym.sym == SDLK_a)
-				{
-					keyA = 0;
-				}
-				// ONLY RIGHT
-				else if (sdlEvent.key.keysym.sym == SDLK_d)
-				{
-					keyD = 0;
-				}
-				break;
-			*/
+				/*
+				case SDL_KEYDOWN:
+					if (sdlEvent.key.keysym.sym == SDLK_ESCAPE)
+					{
+						keepRunning = false;
+					}
+					// ONLY UP
+					else if (sdlEvent.key.keysym.sym == SDLK_w)
+					{
+						keyW = 1;
+					}
+					// ONLY DOWN
+					else if (sdlEvent.key.keysym.sym == SDLK_s)
+					{
+						keyS = 1;
+					}
+					// ONLY LEFT
+					else if (sdlEvent.key.keysym.sym == SDLK_a)
+					{
+						keyA = 1;
+					}
+					// ONLY RIGHT
+					else if (sdlEvent.key.keysym.sym == SDLK_d)
+					{
+						keyD = 1;
+					}
+					break;
+				case SDL_KEYUP:
+					// ONLY UP
+					if (sdlEvent.key.keysym.sym == SDLK_w)
+					{
+						keyW = 0;
+					}
+					// ONLY DOWN
+					else if (sdlEvent.key.keysym.sym == SDLK_s)
+					{
+						keyS = 0;
+					}
+					// ONLY LEFT
+					else if (sdlEvent.key.keysym.sym == SDLK_a)
+					{
+						keyA = 0;
+					}
+					// ONLY RIGHT
+					else if (sdlEvent.key.keysym.sym == SDLK_d)
+					{
+						keyD = 0;
+					}
+					break;
+				*/
 			case SDL_MOUSEBUTTONDOWN:
 				if (sdlEvent.button.clicks == SDL_BUTTON_LEFT)
 				{
@@ -231,9 +322,14 @@ int main(int argc, char* argv[])
 		SDL_Rect destinationRect2{ keyboardX, keyboardY, 40, 40 };
 		SDL_Rect destinationRect3{ mouseX - 16, mouseY - 16, 32, 32 };
 
+		// TODO: Font Rect
+		SDL_Rect fontDstRect{ 25, 100, 300, 32 };
+		SDL_RenderCopy(g_sdlRenderer, textTexture, NULL, &fontDstRect);
+
 		// TODO: Copy the texture onto the rendering target
-		SDL_RenderCopy(g_sdlRenderer, doorTexture, NULL, &destinationRect);
-		SDL_RenderCopy(g_sdlRenderer, crusaderTexture, NULL, &destinationRect2);
+		//SDL_RenderCopy(g_sdlRenderer, doorTexture, NULL, &destinationRect);
+		//SDL_RenderCopy(g_sdlRenderer, crusaderTexture, NULL, &destinationRect2);
+		SDL_RenderCopy(g_sdlRenderer, pngTexture, NULL, &destinationRect2);
 		SDL_RenderCopy(g_sdlRenderer, textureWithoutSurface, NULL, &destinationRect3);
 
 		// TODO: Update the screen with the state of the render target
@@ -243,13 +339,13 @@ int main(int argc, char* argv[])
 		SDL_Delay(16);
 	}
 
-	// TODO: Clean up
-	SDL_DestroyTexture(doorTexture);
-	SDL_DestroyTexture(crusaderTexture);
+	SDL_DestroyTexture(pngTexture);
 	SDL_DestroyTexture(textureWithoutSurface);
-	SDL_DestroyRenderer(g_sdlRenderer); // Opposite of SDL_CreateRenderer(...)
-	SDL_DestroyWindow(g_sdlWindow); // Opposite of SDL_CreateWindow(...)
-	SDL_Quit(); // Opposite of SDL_Init(...)
+	Mix_FreeChunk(coinSFX);
+	Mix_FreeMusic(music);
+	SDL_DestroyTexture(textTexture);
+
+	CleanUp();
 
 	return 0;
 }
